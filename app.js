2bc5742321977d4866339636e70ee8d7e266c31d
Brittany LaCoste <lacostebrittany0227@gmail.com>
2017-09-26 20:44:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
   angular
     .module('app', []);
 })();
+
 (function() {
   'use strict';
 
@@ -17,31 +18,30 @@
   function MainController() {
     var vm = this;
     vm.loanBalance = 0;
-      vm.interestRate = 0;
-      vm.loanTerm = 0;
-      vm.period = 0;
-      vm.downPayment = 0;
-      vm.calculateMortgage = function() {
+    vm.interestRate = 0;
+    vm.loanTerm = 0;
+    vm.period = 0;
+    vm.downPayment = 0;
+    vm.calculateMortgage = () => {
 
-        // monthly interest rate
-         var monthlyInterestRate = ((vm.interestRate/100)/vm.period);
+    // monthly interest rate
+    var monthlyInterestRate = ((vm.interestRate/100)/vm.period);
 
-        // number of payments
-        var numberOfPayments = (vm.loanTerm * vm.period);
+    // number of payments
+    var numberOfPayments = (vm.loanTerm * vm.period);
 
-        // compounded interest rate
-        var compoundedInterestRate = Math.pow((1 + monthlyInterestRate),numberOfPayments);
+    // compounded interest rate
+    var compoundedInterestRate = Math.pow((1 + monthlyInterestRate),numberOfPayments);
 
-        // interest quotient
-        var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+    // interest quotient
+    var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
 
-        // final calculation
-        var monthlyPayment = Math.round((vm.loanBalance - vm.downPayment) * interestQuotient).toFixed(2);
+    // final calculation
+    var monthlyPayment = Math.round((vm.loanBalance - vm.downPayment) * interestQuotient).toFixed(2);
 
-        vm.output = monthlyPayment + " a month"
-        preventDefault()
+    vm.output = `${monthlyPayment} a month`;
+    preventDefault()
 
-
-      }
+    }
   }
 })();
